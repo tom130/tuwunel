0070bca3dde81d3fec3cf6eec7eb23361ba7baf2
Jason Volk <jason@zemos.net>
2026-03-02 06:45:25 +0000

    Add sys util to get rss from statm on linux.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/core/utils/sys/usage.rs
+++ src/core/utils/sys/usage.rs
@@ -1,8 +1,40 @@
+use std::{fs::File, io::Read, str};
+
 use nix::sys::resource::Usage;
 #[cfg(unix)]
 use nix::sys::resource::{UsageWho, getrusage};
 
-use crate::Result;
+use crate::{Error, Result, checked};
+
+pub fn rss() -> Result<usize> {
+	statm(0)
+		.and_then(|pages| Ok((pages, super::page_size()?)))
+		.and_then(|(pages, page_size)| checked!(pages * page_size))
+}
+
+#[cfg(target_os = "linux")]
+pub fn statm(idx: usize) -> Result<usize> {
+	File::open("/proc/self/statm")
+		.map_err(Error::from)
+		.and_then(|mut fp| {
+			let mut buf = [0; 64];
+			let len = fp.read(&mut buf)?;
+
+			str::from_utf8(&buf[0..len])?
+				.split_ascii_whitespace()
+				.map(str::parse::<usize>)
+				.nth(idx)
+				.transpose()
+				.map_err(Into::into)
+		})
+		.transpose()
+		.expect("missing contents of statm")
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn statm(idx: usize) -> Result<usize> {
+	Err!("proc_pid_statm(5) only available on linux systems")
+}
 
 #[cfg(unix)]
 pub fn usage() -> Result<Usage> { getrusage(UsageWho::RUSAGE_SELF).map_err(Into::into) }
